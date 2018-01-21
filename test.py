c70edc599d07ec8c3f5f00a107fdb0211d4cbeab
Jack Hessel <jmhessel@gmail.com>
2018-01-21 18:18:13 -0500

    tweaks

--- test.py
+++ test.py
@@ -28,17 +28,38 @@ with open('rec_table.txt', 'w') as f:
 m_supergraph = Supergraph.SuperGraph.readRecommendationTable('rec_table.txt')
 target = m_supergraph.convexCombinationA(10, alpha=.5)
 constraints = m_supergraph.uniformC(10)
-print(target)
+
 m_solver = Solver.Solver(m_supergraph)
 m_solver.solveGreedy(constraints,
                      target,
                      'rec_table.txt')
 GREEDY_edges = m_solver.edges
+v1_max, v2_max = 0, 0
+v1_min, v2_min = 99999999, 99999999
+for e in GREEDY_edges:
+    v1_max = max(v1_max, e[0])
+    v2_max = max(v2_max, e[1])
+    v1_min = min(v1_min, e[0])
+    v2_min = min(v2_min, e[1])
+    
+print(v1_max, v2_max)
+print(v1_min, v2_min)
+
+def convert_edges_to_normal(edges):
+    return [(e[0]-1, e[1]-n_users-1) for e in edges]
+
+GREEDY_edges = convert_edges_to_normal(m_solver.edges)
+
+#okay -- edges are encoded in a weird way.
+#the first value is the 1-indexed user
+#the second value is the 1-indexed item + N_users.
+
+quit()
 print(len(GREEDY_edges))
 m_solver.solveWithGoal(constraints,
                        target,
                        'dmx.problem',
                        'rec_table.txt')
-GOL_edges = m_solver.edges
+GOL_edges = convert_edges_to_normal(m_solver.edges)
 print(len(GOL_edges))
 quit()
